ebd5d64e8340d2b194580446e0a9fe17b31be49f
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-08 19:42:19 +0300

    Create get notes function

--- notes_project/api/views.py
+++ notes_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Note
+from .serializer import NoteSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_notes(request):
+    notes = Note.objects.all()
+    serialized_data = NoteSerializer(notes, many=True).data
+    return Response(serialized_data)        
