49597802bb58af9c05fdcd503dbc8982989ed339
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-08 20:05:06 +0300

    Implement create_note function

--- notes_project/api/views.py
+++ notes_project/api/views.py
@@ -9,3 +9,15 @@ def get_notes(request):
     notes = Note.objects.all()
     serialized_data = NoteSerializer(notes, many=True).data
     return Response(serialized_data)        
+
+
+@api_view(['POST'])
+def create_note(request):
+    data = request.data
+    serializer = NoteSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
