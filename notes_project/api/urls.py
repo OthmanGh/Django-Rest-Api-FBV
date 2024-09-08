1e94ef84ee3f675f6d7707643d917e7f3983b899
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-08 20:05:41 +0300

    Add create_note func to path

--- notes_project/api/urls.py
+++ notes_project/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.get_notes, name='get_notes')
+    path('notes/', views.get_notes, name='get_notes'),
+    path('notes/create', views.create_note, name='create_note')
 ]
