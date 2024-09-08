2d15a5506244d52ea5e407cc5777d39a6d5fc81a
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-08 22:27:55 +0300

    add note_detail to path urls

--- notes_project/api/urls.py
+++ notes_project/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.get_notes, name='get_notes'),
-    path('notes/create', views.create_note, name='create_note')
+    path('notes/create', views.create_note, name='create_note'),
+    path('notes/<int:pk>', views.note_detail, name='note_detail')
 ]
