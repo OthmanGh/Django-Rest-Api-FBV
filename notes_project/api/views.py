cf197af09568a4f27b2457a33e3ef1f76b9834b1
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-08 22:27:05 +0300

    Implement update & delete note functionality

--- notes_project/api/views.py
+++ notes_project/api/views.py
@@ -10,7 +10,6 @@ def get_notes(request):
     serialized_data = NoteSerializer(notes, many=True).data
     return Response(serialized_data)        
 
-
 @api_view(['POST'])
 def create_note(request):
     data = request.data
@@ -21,3 +20,23 @@ def create_note(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def note_detail(request, pk):  
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = NoteSerializer(note, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
